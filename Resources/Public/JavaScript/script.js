b0fa0b53ba53a8942a46d50fb546f67b6e8736c2
Sebastian Michaelsen <sebastian@michaelsen.io>
2016-04-14 16:03:35 +0200

    [FEATURE] .active for clicked links

--- Resources/Public/JavaScript/script.js
+++ Resources/Public/JavaScript/script.js
@@ -2,10 +2,16 @@
 	$.fn.categoryNavigation = function () {
 		return this.each(function () {
 			var categoryNavigation = $(this);
-            categoryNavigation.parent().find('[data-categories]').hide();
-			categoryNavigation.find('[data-target-category]').on('click', function() {
-				var clickedCategory = ',' + $(this).data('targetCategory') + ',';
-				categoryNavigation.parent().find('[data-categories]').hide().each(function() {
+            var categoryLinks = categoryNavigation.find('[data-target-category]');
+            var targets = categoryNavigation.parent().find('[data-categories]');
+
+            targets.hide();
+            categoryLinks.on('click', function() {
+                var clickedLink = $(this);
+                categoryLinks.removeClass('active');
+                clickedLink.addClass('active');
+				var clickedCategory = ',' + clickedLink.data('targetCategory') + ',';
+				targets.hide().each(function() {
 					var targetElement = $(this);
 					var categories = ',' + targetElement.data('categories');
 					if (categories.indexOf(clickedCategory) > -1) {
